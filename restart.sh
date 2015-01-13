7dc36bce0a93bf7cf86c91d8fcc7d497b8ceb39c
Sriram Velamur <sriram.velamur@gmail.com>
2015-01-13 17:03:52 +0530

    Adds sourcing call to update Python's working environment

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 function restart() {
     pkill python
-    nohup python /root/feedpuller/puller.py > /root/latest-pull.log 2>&1&
+    source /etc/profile;nohup python /root/feedpuller/puller.py > /root/latest-pull.log 2>&1&
 }
 
 restart
