85c61c3da9aeaf1fc38664f3694d937dcaf76a59
Sriram Velamur <sriram.velamur@gmail.com>
2015-01-16 23:07:56 +0530

    Changes kill to only puller.py running Python proc
    Some instances had other python processes killed due to a blanket
    kill - python call. Commit fixes it.

--- restart.sh
+++ restart.sh
@@ -1,5 +1,11 @@
 function restart() {
-    pkill python
+    process=`ps aux|grep [p]uller.py|awk '{print $2}'`
+    if [[ ${#process} -ne 0 ]]
+    then
+        kill $process
+    fi
+    # Oddly the -w/-n checks seem to fail in the process list check.
+    # So, a string length check is added.
     source /etc/profile;nohup python /root/feedpuller/puller.py > /root/latest-pull.log 2>&1&
 }
 
