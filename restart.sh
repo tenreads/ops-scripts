bba107e78246bdab3d29ace477ffa3fae12aae37
Sriram Velamur <sriram.velamur@gmail.com>
2015-01-18 00:20:47 +0530

    Updates restart script puller path

--- restart.sh
+++ restart.sh
@@ -1,12 +1,12 @@
 function restart() {
-    process=`ps aux|grep [p]uller.py|awk '{print $2}'`
+    process=`ps aux|grep "parser.py"|awk '{print $2}'`
     if [[ ${#process} -ne 0 ]]
     then
         kill $process
     fi
     # Oddly the -w/-n checks seem to fail in the process list check.
     # So, a string length check is added.
-    source /etc/profile;nohup python /root/feedpuller/puller.py > /root/latest-pull.log 2>&1&
+    source /etc/profile;nohup python /root/python-node-feedpuller/feed_parser.py > /root/latest-pull.log 2>&1&
 }
 
 restart
